c8117e42c8a7f34cf0b48cf58b2338156dfc30dd
Mutant Eel <mutanteel@gmail.com>
2013-07-29 18:12:12 -0700

    fixed vlm project issue with bad paths, created a dummy function to get compiled so the framework has a library to link with.

---
+++ projects/mac/glm/dummy.c
@@ -0,0 +1,14 @@
+//
+//  dummy.c
+//  glm
+//
+//  Created by Sean on 7/29/13.
+//  Copyright (c) 2013 Mutant Eel. All rights reserved.
+//
+
+#include <stdio.h>
+
+void dummy_function_for_xcode_to_create_a_library()
+{
+	
+}
